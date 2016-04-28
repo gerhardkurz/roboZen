84bddb30f82b1ac9ed80e77691a4a114d98c263e
Florian Gauger <Flo.Gauger@gmail.com>
2016-04-28 15:41:01 +0200

    Move krislet files to project

--- java/src/main/java/edu/kit/robocup/SoccerInterfaces.java
+++ java/src/main/java/edu/kit/robocup/SoccerInterfaces.java
@@ -1,4 +1,4 @@
-//
+package edu.kit.robocup;//
 //	File:			SoccerInterfaces.java
 //	Author:		Krzysztof Langner
 //	Date:			1997/04/28
@@ -9,37 +9,39 @@
 //      Added the bye command
 
 
-
 //***************************************************************************
 //
 //	This interface declares functions which are used to send
 //	command to player
 //
 //***************************************************************************
-interface SendCommand
-{
+interface SendCommand {
     // This function sends move command to the server
     void move(double x, double y);
+
     // This function sends turn command to the server
     void turn(double moment);
+
     void turn_neck(double moment);
+
     // This function sends dash command to the server
     void dash(double power);
+
     // This function sends kick command to the server
     void kick(double power, double direction);
+
     // This function sends say command to the server
     void say(String message);
+
     // This function sends chage_view command to the server
     void changeView(String angle, String quality);
+
     // This function sends a bye command to the server
     void bye();
 }
 
 
-
-
-interface SensorInput
-{
+interface SensorInput {
     //---------------------------------------------------------------------------
     // This function sends see information
     public void see(VisualInfo info);
---
+++ java/src/main/java/edu/kit/robocup/SoccerParams.java
@@ -0,0 +1,10 @@
+package edu.kit.robocup;//
+//	File:			edu.kit.robocup.SoccerParams.java
+//	Author:		Krzysztof Langner
+//	Date:			1997/04/28
+//
+
+class SoccerParams {
+    final static int simulator_step = 100;
+}
+
