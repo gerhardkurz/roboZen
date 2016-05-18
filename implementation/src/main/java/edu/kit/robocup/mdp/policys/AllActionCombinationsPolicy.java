c4baa03cfcbcdd0c385c676a628f56aa624648dc
daniela <grimmdani93@googlemail.com>
2016-05-18 11:37:36 +0200

    added policy with all action combinations again

---
+++ implementation/src/main/java/edu/kit/robocup/mdp/policys/AllActionCombinationsPolicy.java
@@ -0,0 +1,48 @@
+package edu.kit.robocup.mdp.policys;
+
+import edu.kit.robocup.game.*;
+import edu.kit.robocup.game.controller.IPlayerController;
+import edu.kit.robocup.mdp.IPolicy;
+import edu.kit.robocup.mdp.IState;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by dani on 18.05.2016.
+ */
+public class AllActionCombinationsPolicy implements IPolicy {
+    public Map<IPlayerController, IAction> apply(IState state, List<? extends IPlayerController> players) {
+        Map<IPlayerController, IAction> action = new HashMap<>();
+        for (IPlayerController playerController : players) {
+            if (yoloGen(0.25f)) {
+                if (yoloGen(0.25f)) {
+                    int p = 0 + (int)(Math.random() * ((100 - 0) + 1));
+                    int a = 0 + (int)(Math.random() * ((180 - 0) + 1));
+                    action.put(playerController, new Kick(p, a));
+                } else {
+                    int a = 0 + (int)(Math.random() * ((180 - 0) + 1));
+                    action.put(playerController, new Turn(a));
+                }
+            } else {
+                if (yoloGen(0.25f)) {
+                    int x = 0 + (int)(Math.random() * ((50 - 0) + 1));
+                    int y = 0 + (int)(Math.random() * ((30 - 0) + 1));
+                    action.put(playerController, new Move(x, y));
+                } else {
+                    int p = 0 + (int)(Math.random() * ((100 - 0) + 1));
+                    action.put(playerController, new Dash(p));
+                }
+            }
+        }
+        return action;
+    }
+
+
+    private boolean yoloGen(float probability) {
+        Random rnd = new Random();
+        return rnd.nextFloat() <= probability;
+    }
+}
