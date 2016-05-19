079cd2e39caae429e545d855ac7b5e7d8ab0320b
daniela <grimmdani93@googlemail.com>
2016-05-19 20:51:25 +0200

    updated constants

--- implementation/src/main/java/edu/kit/robocup/constant/Constants.java
+++ implementation/src/main/java/edu/kit/robocup/constant/Constants.java
@@ -4,8 +4,18 @@ package edu.kit.robocup.constant;
 
 public class Constants {
     public static final double KICKABLE_MARGIN = 0.7;
-    public static final double PITCH_WIDTH = 105;
-    public static final double GOAL_WIDTH = 14;
-    public static final Goal GOAL_EAST = new Goal(PITCH_WIDTH / 2.0, GOAL_WIDTH);
-    public static final Goal GOAL_WEST = new Goal(-PITCH_WIDTH / 2.0, GOAL_WIDTH);
+    public static final double PITCH_LENGTH = 105;
+    public static final double PITCH_WIDTH = 68;
+    public static final double GOAL_WIDTH = 14.02;
+    public static final double PITCH_MARGIN = 5.0;
+    public static final double CENTER_CIRCLE_R  =9.15;
+    public static final double PENALTY_AREA_LENGTH =16.5;
+    public static final double PENALTY_AREA_WIDTH =40.32;
+    public static final double GOAL_AREA_LENGTH = 5.5;
+    public static final double GOAL_AREA_WIDTH = 18.32;
+    public static final double GOAL_DEPTH = 2.44;
+    public static final double PENALTY_SPOT_DIST =11.0;
+    public static final double CORNER_ARC_R = 1.0;
+    public static final Goal GOAL_EAST = new Goal(PITCH_LENGTH / 2.0, GOAL_WIDTH);
+    public static final Goal GOAL_WEST = new Goal(-PITCH_LENGTH / 2.0, GOAL_WIDTH);
 }
