6d15589a654d759496ae2ec812f6ebfa4ce09479
Andreas Mikolajewski <andi_miko@yahoo.de>
2016-06-03 05:43:13 +0200

    GameReader now reads PlayerActionSets instead of ActionSets. Now you know which Player performed an Action. Adjust usage of Game needed.

--- implementation/src/main/java/edu/kit/robocup/game/PlayerAction.java
+++ implementation/src/main/java/edu/kit/robocup/game/PlayerAction.java
@@ -6,14 +6,40 @@ import java.io.Serializable;
 
 
 public class PlayerAction implements Serializable {
-    int playerNumber;
-    String teamName;
-    IAction action;
+
+
+    private int playerNumber;
+    private String teamName;
+    private IAction action;
 
     public PlayerAction(int playerNumber, String teamName, IAction action) {
         this.playerNumber = playerNumber;
         this.teamName = teamName;
         this.action = action;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+    public IAction getAction() {
+        return action;
+    }
+
+    public void setAction(IAction action) {
+        this.action = action;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
     }
 
     @Override
