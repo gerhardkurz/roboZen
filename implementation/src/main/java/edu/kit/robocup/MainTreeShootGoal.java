83205c026563f6cd7fab9db3bc0628cd1c8dcac3
daniela <grimmdani93@googlemail.com>
2016-06-27 16:28:44 +0200

    finally got a version where the team is able to shoot a goal!! but it takes a while...

---
+++ implementation/src/main/java/edu/kit/robocup/MainTreeShootGoal.java
@@ -0,0 +1,51 @@
+package edu.kit.robocup;
+
+import edu.kit.robocup.constant.PitchSide;
+import edu.kit.robocup.game.PlayMode;
+import edu.kit.robocup.game.controller.Team;
+import edu.kit.robocup.game.controller.Trainer;
+import edu.kit.robocup.game.state.Ball;
+import edu.kit.robocup.game.state.PlayerState;
+import edu.kit.robocup.util.Util;
+import edu.kit.robocup.mdp.policy.DoNothing;
+import edu.kit.robocup.mdp.policy.PerPlayModePolicy;
+import edu.kit.robocup.mdp.policy.heurisic.BeforeGamePolicy;
+import edu.kit.robocup.mdp.policy.heurisic.KickOffPolicy;
+import edu.kit.robocup.tree.TreePolicy;
+
+/**
+ * Created by dani on 27.06.2016.
+ */
+public class MainTreeShootGoal {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        PerPlayModePolicy perPlayModePolicy = new PerPlayModePolicy(new TreePolicy());
+        perPlayModePolicy.replacePolicyForPlayMode(new KickOffPolicy(), PlayMode.KICK_OFF_EAST, PlayMode.KICK_OFF_WEST, PlayMode.GOAL_SIDE_EAST, PlayMode.GOAL_SIDE_WEST);
+        perPlayModePolicy.replacePolicyForPlayMode(new BeforeGamePolicy(), PlayMode.UNKNOWN);
+
+        PerPlayModePolicy perPlayModePolicy1 = new PerPlayModePolicy(new DoNothing());
+        perPlayModePolicy1.replacePolicyForPlayMode(new KickOffPolicy(), PlayMode.KICK_OFF_EAST, PlayMode.KICK_OFF_WEST, PlayMode.GOAL_SIDE_EAST, PlayMode.GOAL_SIDE_WEST);
+        perPlayModePolicy1.replacePolicyForPlayMode(new BeforeGamePolicy(), PlayMode.UNKNOWN);
+
+        Util.initEnvironment();
+
+        Trainer trainer = new Trainer("Trainer");
+        trainer.connect();
+        Team team1 = new Team(PitchSide.EAST, 2, perPlayModePolicy);
+        team1.connectAll();
+
+        Team team2 = new Team(PitchSide.WEST, 2, perPlayModePolicy1);
+        team2.connectAll();
+
+        trainer.movePlayer(new PlayerState(PitchSide.WEST, 1, -50, 50));
+        trainer.movePlayer(new PlayerState(PitchSide.WEST, 2, -50, -50));
+
+        trainer.movePlayer(new PlayerState(PitchSide.EAST, 1, 5, -5));
+        trainer.movePlayer(new PlayerState(PitchSide.EAST, 2, 5, 5));
+
+        Thread.sleep(1000);
+        trainer.moveBall(new Ball(5, -5));
+        trainer.changePlayMode(com.github.robocup_atan.atan.model.enums.PlayMode.PLAY_ON);
+    }
+}
